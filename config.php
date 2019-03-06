8d1e90652666fd4dd05fd68c267722f8b8a235f2
joshwren <joshuawren@outlook.com>
2019-03-06 21:14:22 +1100

    .

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database config
+ */
+$host       = "localhost";
+$username   = "root";
+$password   = "root";
+$dbname     = "ben_test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+?>
---
+++ public/templates/header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+
+<head>
+
+    <title>Ben's Collection</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Ben's Collection tracker</h1>
